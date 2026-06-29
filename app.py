7c4ad527e3e706c9717f25a244d81aa22b7566a0
Noel Kurian <nbkurian11@gmail.com>
2026-06-29 17:02:58 -0400

    added POST /debts endpoint

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from database import init_db
+import sqlite3
+
 
 app = Flask(__name__)
+init_db()
 
 @app.route("/")
 def greeting():
     return "ZeroDay is running"
+
+
+@app.route("/debts", methods = ["POST"])
+def add_debt():
+    data = request.get_json()
+    name = data.get("name")
+    balance = data.get("balance")
+    interest_rate = data.get("interest_rate")
+    minimum_payment = data.get("minimum_payment")
+
+    connection = sqlite3.connect("zeroday.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO debts (name, balance, interest_rate, minimum_payment) VALUES (?, ?, ?, ?)",
+    (name, balance, interest_rate, minimum_payment)
+    )
+    connection.commit()
+    connection.close()
+
 
 
 if __name__ == "__main__":
